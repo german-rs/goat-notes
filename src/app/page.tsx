f8de45ade4615ba36e4f692ca0f34820790511e1
german-rs <ing.germanriveros@gmail.com>
2025-05-26 14:08:04 -0400

    layout, utils, y header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function HomePage(){
   return(
-    <div className="h-20 bg-red-400 text-blue-300">Home Page</div>
-  )
+    <div>Home Page</div>
+  );
 }
 
-export default HomePage
+export default HomePage;
